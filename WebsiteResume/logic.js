726a154f78a025bb7a2dd14ea8fa9a6f8a487ce6
Andy Zhang <67079259+azhang777@users.noreply.github.com>
2023-01-16 19:37:58 -0500

    13
    I would like to say I am 95 percent done with the layout of this project. Colors can be interchangeable. Though the template I worked with to create this project allowed me to get the grasp of CSS and HTML, I believe it restricts me from putting more creative expression into my web design. This template was inspired by https://www.youtube.com/watch?v=hnjHCmaUVPg&t=1856s&ab_channel=OnlineTutorials. I am grateful for the tutorial as it allowed me to get into  using html and css together.

--- WebsiteResume/logic.js
+++ WebsiteResume/logic.js
@@ -1,3 +1,89 @@
+let javaSkill = document.getElementById("java");
+let javaSkillLevel = document.getElementById("java_level");
+
+var skillLevel = document.getElementsByClassName("skill_level");
+var skillText = document.getElementsByClassName("skills_text");
+for (let i = 0; i < skillText.length; i++) {
+  skillText[i].addEventListener("mouseover", function () {
+    skillText[i].style.animation = "none";
+    skillLevel[i].classList.add("appear");
+  });
+  skillText[i].addEventListener("animationend", function () {
+    skillLevel[i].classList.remove("appear");
+    skillLevel[i].style.opacity = 1;
+  });
+}
+
+console.log(skillLevel);
+console.log(skillText);
+/*
++let javaSkill = document.getElementById("java");
+let javaSkillLevel = document.getElementById("java_level");
+
+javaSkill.addEventListener("mouseover", function () {
+  javaSkillLevel.classList.add("appear");
+});
+
+javaSkill.addEventListener("mouseout", function () {
+  javaSkillLevel.classList.remove("appear");
+});
+
+let javascriptSkill = document.getElementById("javascript");
+let javascriptSkillLevel = document.getElementById("javascript_level");
+
+javascriptSkill.addEventListener("mouseover", function () {
+  javascriptSkillLevel.classList.add("appear");
+});
+
+javascriptSkill.addEventListener("mouseout", function () {
+  javascriptSkillLevel.classList.remove("appear");
+});
+
+let cplusplusSkill = document.getElementById("cplusplus");
+let cplusplusSkillLevel = document.getElementById("cplusplus_level");
+
+cplusplusSkill.addEventListener("mouseover", function () {
+  cplusplusSkillLevel.classList.add("appear");
+});
+
+cplusplusSkill.addEventListener("mouseout", function () {
+  cplusplusSkillLevel.classList.remove("appear");
+});
+
+let htmlSkill = document.getElementById("html");
+let htmlSkillLevel = document.getElementById("html_level");
+
+htmlSkill.addEventListener("mouseover", function () {
+  htmlSkillLevel.classList.add("appear");
+});
+
+htmlSkill.addEventListener("mouseout", function () {
+  htmlSkillLevel.classList.remove("appear");
+});
+
+let cssSkill = document.getElementById("css");
+let cssSkillLevel = document.getElementById("css_level");
+
+cssSkill.addEventListener("mouseover", function () {
+  cssSkillLevel.classList.add("appear");
+});
+
+cssSkill.addEventListener("mouseout", function () {
+  cssSkillLevel.classList.remove("appear");
+});
+
+let mysqlSkill = document.getElementById("mySQL");
+let mysqlSkillLevel = document.getElementById("mySQL_level");
+
+mysqlSkill.addEventListener("mouseover", function () {
+  mysqlSkillLevel.classList.add("appear");
+});
+
+mysqlSkill.addEventListener("mouseout", function () {
+  mysqlSkillLevel.classList.remove("appear");
+});
+/*
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -30,3 +116,4 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+*/
